122266c37b056a0fda907ec375936031d20cb747
dream333wxrld-juice <dream333wxrld@gmail.com>
2026-03-11 15:12:35 +0300

    Enhance assignment.py with print statements and explanations
    Added print statements for various questions and included a bonus question explaining lists and dictionaries.

--- assignment.py
+++ assignment.py
@@ -1,1 +1,22 @@
+# Question 4
+print("Hello AI Student")
+# Question 5 
+Name="john"
+Age=20
+City="nairobi"
+print("Name")
+print("Age") 
+print("City")
+# Question 6
+language=["Python", "Java", "JavaScript", "Ruby","C++",]
+print(language[0])
+# Question 7
+Print the student's name.
+student={"name":"gilbert",
+         "age":"19",
+         "course":"AI",
+        }
+# Bonus Question (Optional)
+a list is an ordered collection of items that can be accessed by their position while
+a  dictionary is a collection of key-value pairs where the key is used to access its corresponding value
 
